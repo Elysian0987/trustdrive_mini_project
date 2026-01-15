f9ee55a64f7e491330612c272e620faad12ed6c8
Samrudhi Bhat <samrudhibhat29@gmail.com>
2026-01-15 16:50:20 +0530

    Refactor code structure for improved readability and maintainability

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,8 +11,8 @@ module.exports = {
   // solidity: "0.8.18",
   networks: {
     sepolia: {
-      url: SEPOLIA_URL,
-      accounts: [PRIVATE_KEY],
+      url: 'https://eth-sepolia.g.alchemy.com/v2/isOgrECpuP6ehS-Ii9bKsb1a2zT2lI5O',
+      accounts: [eec54233ea9e47dcbfc0d93d2707d5013bc0e53c3dff381693b67530a26cffaf],
     }
   },
   solidity: {
